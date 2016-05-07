39e21a5ff4d22ac2dc9935ec09f530783c27b44b
gorzan <gorzan@gmail.com>
2016-05-07 18:41:10 +0200

    flask

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 from flask import Flask
+import requests
+import simplejson
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    uri = 'https://api.bitcoinaverage.com/ticker/USD/'
+    try:
+    	uResponse = requests.get(uri)
+	except requests.ConnectionError:
+		return "Connection Error"
+	Jresponse = uResponse.text
+	data = json.loads(Jresponse)
+
+    return data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
