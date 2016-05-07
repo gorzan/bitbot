462cdbf0921f4d42fd807a1ec02d41ffbfd3e39a
gorzan <gorzan@gmail.com>
2016-05-08 00:26:35 +0200

    Bitcoin prototype

--- test.py
+++ test.py
@@ -9,12 +9,17 @@ app = Flask(__name__)
 def home():
     uri = 'https://api.bitcoinaverage.com/ticker/USD/'
     try:
-        uResponse = requests.get(uri)
+       uResponse = requests.get(uri)
     except requests.ConnectionError:
        return "Connection Error"
     Jresponse = uResponse.text
     data = json.loads(Jresponse)
-    return data
+
+    last = data['last']
+
+    result = 'Siste transaksjonskurs Bitcoin: $' + str(last)
+
+    return result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
