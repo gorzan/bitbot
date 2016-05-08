eb2581d3bfde034b765faff7e6fe4510dd87bff5
gorzan <gorzan@gmail.com>
2016-05-08 14:27:35 +0200

    endret til å kun benytte ett api for pris, mulighet for alle cryptocurrencies som ligger der

--- bitbot.py
+++ bitbot.py
@@ -7,40 +7,30 @@ app = Flask(__name__)
 
 @app.route('/price', methods=['POST', 'GET'])
 def price():
-	#define uri for various tickers
-	api_uri = {}
-	api_uri['btc'] = 'https://api.bitcoinaverage.com/ticker/USD/'
-	api_uri['eth'] = 'https://coinmarketcap-nexuist.rhcloud.com/api/eth'
-
 	#check if ticker is defined
 	if 'text' not in request.args:
 		return "Ingen ticker definert. Bruk '/price [ticker]'."
 
 	ticker = request.args.get('text').lower()
+	coin_uri = 'https://coinmarketcap-nexuist.rhcloud.com/api/' + ticker
 
-	#check if valid ticker
-	if ticker not in api_uri.keys():
-		return "Ukjent ticker '" + ticker.upper() +"'"
 	
 	#get appropriate json object for ticker and put it in data
 	try:
-	   uResponse = requests.get(api_uri[ticker])
+	   uResponse = requests.get(coin_uri)
 	except requests.ConnectionError:
 		return "Tilkoblingsfeil - klarer ikke hente kurs for " + ticker.upper()
 	Jresponse = uResponse.text
 	data = json.loads(Jresponse)
 
+	#check ticker existence
+	if 'name' not in data:
+		return "Ukjent ticker: '" + ticker.upper() + "'"
+
 	#create response dict
 	output = {}
 	output['response_type'] = 'in_channel'
-
-	#fetch relevant object from json and build responsetext
-	if ticker == 'btc':
-		output['text'] = 'Siste transaksjonskurs for Bitcoin: $' + str(round(data['last'],2))
-	elif ticker == 'eth':
-		output['text'] = 'Siste transaksjonskurs for Ethereum: $' + str(round(data['price']['usd'],2))
-	else:
-		return "Ukjent ticker '" + ticker.upper() +"'"
+	output['text'] = 'Siste transaksjonskurs for ' + data['name'] + ': $' + str(data['price']['usd'])
 
 	json_response = json.dumps(output)
 
